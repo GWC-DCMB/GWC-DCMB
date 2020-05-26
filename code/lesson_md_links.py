dbc6540dc8bc14a8db1aadf7f53cd21fbcf6c639
Kelly Sovacool <kellysovacool@gmail.com>
2020-05-26 10:33:21 -0400

    Use colab links for jupyter notebooks

--- code/lesson_md_links.py
+++ code/lesson_md_links.py
@@ -1,5 +1,5 @@
 #!/usr/local/bin/python3
 import os
-for file in sorted(os.listdir("Lessons")): 
+for file in sorted(os.listdir("Lessons")):
     if "." in file and not file.startswith('.'):
-        print(f"- [{' '.join(file.split('.')[0].split('_'))}]({file})")
+        print(f"- [{' '.join(file.split('.')[0].split('_'))}](https://colab.research.google.com/github/GWC-DCMB/SummerExperience/blob/master/Lessons/{file})")
